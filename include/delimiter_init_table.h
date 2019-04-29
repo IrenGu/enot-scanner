5a2c3e2065e022604c7ee02ec7c445e2a79973d6
IrenGu <48347667+IrenGu@users.noreply.github.com>
2019-04-29 19:57:04 +0300

    Add files via upload

--- include/delimiter_init_table.h
+++ include/delimiter_init_table.h
@@ -11,7 +11,7 @@
 #define DELIMITER_INIT_TABLE_H
 #   include <cstddef>
 #   include "../include/state_for_char.h"
-namespace lynx_scanner{
+namespace enot_scanner{
     extern const State_for_char delimiter_init_table[];
     extern const std::size_t    delimiter_init_table_size;
 };
